67465c4cd471161a559b96dc8d004d67a33c73a3
kaiolindo <129184951+kaiolindo@users.noreply.github.com>
2024-09-12 09:25:00 -0300

    1

--- main.js
+++ main.js
@@ -1,5 +1,37 @@
 const caixaPrincipal = document.querySelector(".caixa-principal");
-const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaPergunta = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
+
+let perguntas = [
+    {
+        pergunta1: "texto da pergunta",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+{
+        pergunta2: "texto da pergunta",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+
+     {
+        pergunta3: "texto da pergunta",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+     {
+        pergunta4: "texto da pergunta",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+
+];
